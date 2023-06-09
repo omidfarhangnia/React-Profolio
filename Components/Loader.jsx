c7cb1fff6aa6628362ed77237c39aa2ea10fbff5
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-06-09 15:26:26 +0330

    adding the style of loading for page

--- Components/Loader.jsx
+++ Components/Loader.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { gsap } from "gsap";
 
-const Loader = () => {
+const Loader = ({ setIsLoaded }) => {
   function counter(id, start, end, duration) {
     let obj = document.querySelector(id),
       current = start,
@@ -9,7 +9,9 @@ const Loader = () => {
       step = Math.abs(Math.floor(duration / range)),
       timer = setInterval(() => {
         current += 2;
-        obj.textContent = current;
+        if (obj) {
+          obj.textContent = current;
+        }
         if (current == end) {
           clearInterval(timer);
         }
@@ -17,14 +19,165 @@ const Loader = () => {
   }
 
   useEffect(() => {
-    counter(".counterNumber", 0, 100, 4000);
-  }, [])
+    const binaryNumber = document.querySelectorAll(".binaryNumber");
+    let unsubscribe = () => {
+      binaryNumber.forEach((binaryNumberItem) => {
+        const innerHeight = window.innerHeight,
+          innerWidth = window.innerWidth;
+
+        const randomTopNumber = Math.floor(Math.random() * innerHeight - 250);
+        const randomLeftNumber = Math.floor(Math.random() * innerWidth - 50);
+        const randomRotateNumber = Math.floor(Math.random() * 180 - 90);
+        const randomFontNumber = Math.floor(Math.random() * 350 + 150);
+
+        gsap.set(binaryNumberItem, {
+          top: `${randomTopNumber}px`,
+          left: `${randomLeftNumber}px`,
+          rotate: `${randomRotateNumber}deg`,
+          fontSize: `${randomFontNumber}px`,
+          opacity: 0,
+        });
+      });
+    };
+
+    return () => {
+      unsubscribe();
+      let binaryTl = gsap.timeline({
+        onStart: () => {
+          counter(".counterNumber", 0, 100, 4000);
+        },
+        onComplete: () => {
+          setIsLoaded(true);
+        },
+      });
+      binaryTl
+        .to(".counterNumberContainer", {
+          opacity: 0,
+          delay: 4,
+          duration: 0.5,
+          ease: "circ.out",
+        })
+        .to(".binaryNumber", {
+          duration: 1.5,
+          opacity: 1,
+          stagger: 0.03,
+          ease: "power4.out",
+        })
+        .to(
+          ".greenLayer",
+          {
+            opacity: 1,
+            duration: 0.5,
+            ease: "circ.out",
+          },
+          "-=1"
+        );
+    };
+  }, []);
 
   return (
-    <div className="w-full h-[100vh] overflow-hidden bg-black text-customGreen1 select-none text-[80px] md:text-[100px] lg:text-[130px] grid place-items-center relative font-BIZ">
-      <div className="w-[250px] h-[250px] absolute flex items-center justify-center">
-        <span className="counterNumber">0</span><span className="counterPercent">%</span>
+    <div className="w-full h-[100vh] overflow-hidden bg-black text-customGreen2 select-none text-[70px] md:text-[110px] lg:text-[150px] grid place-items-center relative font-BIZ">
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <span className="absolute binaryNumber">0</span>
+      <span className="absolute binaryNumber">1</span>
+      <div className="absolute flex items-center justify-center counterNumberContainer">
+        <span className="counterNumber">0</span>
+        <span className="counterPercent">%</span>
       </div>
+      <div className="w-full h-full bg-customGreen2 greenLayer opacity-0"></div>
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,12 @@
 import Loader from "@/Components/Loader";
 import Context from "@/context/context";
 import "@/styles/globals.css";
-import { createContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/font.css";
 import { gsap } from "gsap";
 
 export default function App({ Component, pageProps }) {
-  const [loaded, setIsLoaded] = useState(true);
-
-  useEffect(() => {
-    // let loaderTl = gsap.timeline({onComplete: () => {
-    //   setIsLoaded(true)
-    // }});
-    // loaderTl
-    // .to(".loaderBineriesNumber", {
-    //   opacity: 1,
-    //   duration: 1,
-    //   stagger: .03,
-    //   ease: "power4.out"
-    // })
-    // .to(".loaderGreenBg", {
-    //   opacity: 1,
-    // })
-  }, []);
+  const [loaded, setIsLoaded] = useState(false);
 
   return (
     <>
@@ -33,7 +17,7 @@ export default function App({ Component, pageProps }) {
           </Context>
         </>
       ) : (
-        <Loader />
+        <Loader setIsLoaded={setIsLoaded}/>
       )}
     </>
   );
