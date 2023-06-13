e3466470c2b7d00dc4cc5c0fe3fa05c643a9f48e
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-06-13 13:43:51 +0330

    finishing the style of say hello page

--- Components/SayHelloSection.jsx
+++ Components/SayHelloSection.jsx
@@ -13,8 +13,8 @@ const SayHelloSection = () => {
       <div className="overloads break-words text-customGreen3/80 text-[100px] md:text-[140px] lg:text-[180px] w-full h-full text-center absolute top-[10%] md:top-[8%] left-0 select-none">
         01010111000001101001000010101011100000110100100001010101001010010101001010
       </div>
-      <div className="w-full h-full absolute top-0 left-0 flex flex-col justify-end">
-        <div className="w-full h-[60%] flex justify-center items-center">
+      <div className="w-full h-full absolute top-0 left-0 flex flex-col justify-end gap-5">
+        <div className="w-full h-auto flex justify-center items-center">
           <form
             onSubmit={(e) => {
               e.preventDefault();
@@ -23,28 +23,28 @@ const SayHelloSection = () => {
           >
             {/* this is for name */}
             <div className="w-full flex flex-wrap justify-between items-center">
-              <label className="capitalize w-[50%] text-[14px] md:text-[18px] lg:text-[23px] text-white" htmlFor="firstNameInput">your name : </label>
-              <input className="w-[50%] max-w-[300px] min-w-[200px] px-6 py-2 rounded-full focus:outline-none" type="text" placeholder="first name" id="firstNameInput" />
+              <label className="capitalize w-[50%] text-[14px] md:text-[18px] lg:text-[23px] text-white mb-2 md:mb-0 ml-3 md:ml-0" htmlFor="firstNameInput">your name : </label>
+              <input className="w-[50%] max-w-[300px] text-[14px] md:text-[18px] lg:text-[20px] min-w-[200px] px-6 py-2 rounded-full focus:outline-none" type="text" placeholder="first name" id="firstNameInput" />
             </div>
             {/* this is for last name */}
             <div className="w-full flex flex-wrap justify-between items-center">
-              <label className="capitalize w-[50%] text-[14px] md:text-[18px] lg:text-[23px] text-white" htmlFor="lastNameInput">your last name : </label>
-              <input className="w-[50%] max-w-[300px] min-w-[200px] px-6 py-2 rounded-full focus:outline-none" type="text" placeholder="last name" id="lastNameInput" />
+              <label className="capitalize w-[50%] text-[14px] md:text-[18px] lg:text-[23px] text-white mb-2 md:mb-0 ml-3 md:ml-0" htmlFor="lastNameInput">your last name : </label>
+              <input className="w-[50%] max-w-[300px] text-[14px] md:text-[18px] lg:text-[20px] min-w-[200px] px-6 py-2 rounded-full focus:outline-none" type="text" placeholder="last name" id="lastNameInput" />
             </div>
             {/* this is for email */}
             <div className="w-full flex flex-wrap justify-between items-center">
-              <label className="capitalize w-[50%] text-[14px] md:text-[18px] lg:text-[23px] text-white" htmlFor="emailInput">email : </label>
-              <input className="w-[50%] max-w-[300px] min-w-[200px] px-6 py-2 rounded-full focus:outline-none" type="email" placeholder="email" id="emailInput" />
+              <label className="capitalize w-[50%] text-[14px] md:text-[18px] lg:text-[23px] text-white mb-2 md:mb-0 ml-3 md:ml-0" htmlFor="emailInput">email : </label>
+              <input className="w-[50%] max-w-[300px] text-[14px] md:text-[18px] lg:text-[20px] min-w-[200px] px-6 py-2 rounded-full focus:outline-none" type="email" placeholder="email" id="emailInput" />
             </div>
             {/* this is for message */}
             <div className="w-full flex flex-wrap justify-between items-center">
-              <label className="capitalize w-[50%] text-[14px] md:text-[18px] lg:text-[23px] text-white" htmlFor="messageInput">message : </label>
-              <textarea className="w-[50%] max-w-[300px] min-w-[200px] md:h-[250px] md:rounded-lg px-6 py-2 rounded-full focus:outline-none resize-none" placeholder="your message" id="messageInput"></textarea>
+              <label className="capitalize w-[50%] text-[14px] md:text-[18px] lg:text-[23px] text-white mb-2 md:mb-0 ml-3 md:ml-0" htmlFor="messageInput">message : </label>
+              <textarea className="w-[50%] max-w-[300px] text-[14px] md:text-[18px] lg:text-[20px] min-w-[200px] md:h-[230px] md:rounded-lg px-6 py-2 rounded-full focus:outline-none resize-none" placeholder="your message" id="messageInput"></textarea>
             </div>
             <button className="bg-white text-customGreen2/90 p-3 text-[14px] md:text-[18px] lg:text-[23px] rounded-full uppercase mt-6">send</button>
           </form>
         </div>
-        <footer className="w-full h-[20%] bg-blue-500">this is footer</footer>
+        <footer className="w-full h-[15%] md:h-[10%] bg-blue-500">this is footer</footer>
       </div>
     </section>
   );
