100a543802da8fb6fbeff09860d99d45ac4a1b24
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-06-15 21:39:23 +0330

    adding firebase structure

---
+++ pages/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPz40a7XNzQEY5dCIQFu9ng4NkkUfRqBQ",
+  authDomain: "react-portfolio-bb4a2.firebaseapp.com",
+  projectId: "react-portfolio-bb4a2",
+  storageBucket: "react-portfolio-bb4a2.appspot.com",
+  messagingSenderId: "943760222453",
+  appId: "1:943760222453:web:20f99dca83ccb272b57dbc"
+};
+
+export const app = initializeApp(firebaseConfig);
