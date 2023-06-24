5e2b728098fa7181d1429ee23c79196f437f8c4e
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-06-24 14:58:13 +0330

    making connection between firebase and env

--- pages/firebase.js
+++ pages/firebase.js
@@ -1,14 +1,15 @@
+import { env } from "@/next.config";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPz40a7XNzQEY5dCIQFu9ng4NkkUfRqBQ",
-  authDomain: "react-portfolio-bb4a2.firebaseapp.com",
-  projectId: "react-portfolio-bb4a2",
-  storageBucket: "react-portfolio-bb4a2.appspot.com",
-  messagingSenderId: "943760222453",
-  appId: "1:943760222453:web:20f99dca83ccb272b57dbc",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
